a3899293d37508e6b489de04416bfe5ba6cd8e47
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 14:21:48 -0300

    resources refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
-#could use resources
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
-
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 end
