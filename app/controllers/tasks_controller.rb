a18f0cd408b94f93c46015d6f38b9d210cd6ffe5
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 12:34:01 -0300

    tasks can be edited :)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,10 +12,19 @@ class TasksController < ApplicationController
     task = Task.create tasks_params
     redirect_to task_path(task)
   end
+  def edit
+    @task = Task.find params[:id]
+  end
+
+  def update
+    task = Task.find params[:id]
+    task.update task_params
+    redirect_to task_path(task)
+  end
 
   private
 
-  def tasks_params
+  def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
 
