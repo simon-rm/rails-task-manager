4fab6e716ed552ababbe56ecf2fae9168283562b
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 12:13:15 -0300

    tasks can be added :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 #could use resources
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
   get 'tasks/:id', to: 'tasks#show', as: :task
 end
